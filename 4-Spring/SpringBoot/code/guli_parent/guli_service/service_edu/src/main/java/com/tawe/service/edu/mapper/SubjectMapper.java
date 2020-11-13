87ce42a93eed785ccb190f2c3364ab9c1f9a31f4
v-tawe <v-tawe@microsoft.com>
2020-11-13 14:59:18 +0800

    生成 Subject 相关 code

---
+++ 4-Spring/SpringBoot/code/guli_parent/guli_service/service_edu/src/main/java/com/tawe/service/edu/mapper/SubjectMapper.java
@@ -0,0 +1,16 @@
+package com.tawe.service.edu.mapper;
+
+import com.tawe.service.edu.entity.Subject;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 课程科目 Mapper 接口
+ * </p>
+ *
+ * @author tawe
+ * @since 2020-11-13
+ */
+public interface SubjectMapper extends BaseMapper<Subject> {
+
+}
